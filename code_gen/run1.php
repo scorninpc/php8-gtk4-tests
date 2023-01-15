c49ab6ee29fc103eb48242ccf652715beeed2358
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-01-14 23:29:54 -0300

    added interfaces and structs manualy

--- code_gen/run1.php
+++ code_gen/run1.php
@@ -1,5 +1,40 @@
 <?php
 
+function explodeCamelCase($string)
+{
+	$re = '/(?<=[a-z])(?=[A-Z]) | (?<=[A-Z])(?=[A-Z][a-z])/x';
+	$str = preg_split($re, $string);
+
+	return $str;
+}
+
+function toCamelCase($string) 
+{
+	$parts = explode("_", $string);
+	foreach($parts as $index => $part) {
+		if(in_array($part, [
+				"vbox", "hbox",
+				"vbutton", "hbutton",
+				"hpaned", "vpaned",
+				"vseparator", "hseparator",
+				"vscrollbar", "hscrollbar",
+				"hscale", "vscale"
+			])) {
+			$parts[$index] = strtoupper(substr($part, 0, 2)).substr($part, 2);
+		}
+		elseif(in_array($part, ["hsv"])) {
+			$parts[$index] = strtoupper(substr($part, 0, 3)).substr($part, 3);
+		}
+		else {
+			$parts[$index] = ucfirst($part);
+		}
+	}
+    // $str = str_replace(" ", "", ucwords(str_replace("_", " ", $string)));
+	$str = implode("", $parts);
+
+    return $str;
+}
+
 
 // $a = [1, 2, 3];
 
@@ -134,6 +169,68 @@ foreach($parsed['object'] as $object) {
 	$classes[$name]['methods'] = [];
 }
 
+// Override
+$classes['GtkAccelMap'] = ['c-name' => "GtkAccelMap",'methods' => [],];
+$classes['GtkAccelerator'] = ['c-name' => "GtkAccelerator", 'methods' => [], ];
+$classes['AppChooserWidget'] = ['c-name' => "AppChooserWidget", 'parent'=>"GtkBox", 'methods' => [], ];
+$classes['GtkAppChooser'] = ['c-name' => "GtkAppChooser", 'parent'=>"GtkAppChooserWidget", 'methods' => [], ];
+$classes['GtkBorder'] = ['c-name' => "GtkBorder", 'methods' => [], ];
+$classes['GtkClipboard'] = ['c-name' => "GtkClipboard", 'methods' => [], ];
+$classes['GtkCssSection'] = ['c-name' => "GtkCssSection", 'methods' => [], ];
+$classes['GtkDrag'] = ['c-name' => "GtkDrag", 'methods' => [], ];
+$classes['GtkGrab'] = ['c-name' => "GtkGrab", 'methods' => [], ];
+$classes['GtkColorChooser'] = ['c-name' => "GtkColorChooser", 'is-interface' => TRUE, 'methods' => [], ];
+$classes['GtkEditable'] = ['c-name' => "GtkEditable", 'is-interface' => TRUE, 'methods' => [], ];
+$classes['GtkEventController'] = ['c-name' => "GtkEventController", 'methods' => [], ];
+$classes['GtkFileChooser'] = ['c-name' => "GtkFileChooser", 'is-interface' => TRUE, 'methods' => [], ];
+$classes['GtkScrollable'] = ['c-name' => "GtkScrollable", 'is-interface' => TRUE, 'methods' => [], ];
+$classes['GtkGesture'] = ['c-name' => "GtkGesture", 'methods' => [], ];
+$classes['GtkFileFilter'] = ['c-name' => "GtkFileFilter", 'methods' => [], ];
+$classes['GtkGlArea'] = ['c-name' => "GtkGlArea", 'methods' => [], ];
+$classes['GtkIconInfo'] = ['c-name' => "GtkIconInfo", 'methods' => [], ];
+$classes['GtkImContext'] = ['c-name' => "GtkImContext", 'methods' => [], ];
+$classes['GtkImMulticontext'] = ['c-name' => "GtkImMulticontext", 'parent'=>"GtkImContext", 'methods' => [], ];
+$classes['GtkMain'] = ['c-name' => "GtkMain", 'methods' => [], ];
+$classes['GtkDevice'] = ['c-name' => "GtkDevice", 'methods' => [], ];
+$classes['GtkKeySnooper'] = ['c-name' => "GtkKeySnooper", 'methods' => [], ];
+$classes['GtkModelButton'] = ['c-name' => "GtkModelButton", 'parent'=>"GtkButton", 'methods' => [], ];
+$classes['GtkPadController'] = ['c-name' => "GtkPadController", 'parent'=>"GtkEventController", 'methods' => [], ];
+$classes['GtkPageSetup'] = ['c-name' => "GtkPageSetup", 'methods' => [], ];
+$classes['GtkPaperSize'] = ['c-name' => "GtkPaperSize", 'methods' => [], ];
+$classes['GtkPlacesSidebar'] = ['c-name' => "GtkPlacesSidebar", 'parent'=>"GtkScrolledWindow", 'methods' => [], ];
+$classes['GtkRecentInfo'] = ['c-name' => "GtkRecentInfo", 'methods' => [], ];
+$classes['GtkFileFilter'] = ['c-name' => "GtkFileFilter", 'methods' => [], ];
+$classes['GtkRender'] = ['c-name' => "GtkRender", 'methods' => [], ];
+$classes['GtkPrintContext'] = ['c-name' => "GtkPrintContext", 'methods' => [], ];
+$classes['GtkPrintSettings'] = ['c-name' => "GtkPrintSettings", 'methods' => [], ];
+$classes['GtkRecentFilter'] = ['c-name' => "GtkRecentFilter", 'methods' => [], ];
+$classes['GtkSelection'] = ['c-name' => "GtkSelection", 'methods' => [], ];
+$classes['GtkTargetList'] = ['c-name' => "GtkTargetList", 'methods' => [], ];
+$classes['GtkTargetTable'] = ['c-name' => "GtkTargetTable", 'methods' => [], ];
+$classes['GtkTarget'] = ['c-name' => "GtkTarget", 'methods' => [], ];
+$classes['GtkRcProperty'] = ['c-name' => "GtkRcProperty", 'methods' => [], ];
+$classes['GtkTest'] = ['c-name' => "GtkTest", 'methods' => [], ];
+$classes['GtkShortcutLabel'] = ['c-name' => "GtkShortcutLabel", 'parent'=>"GtkWidget", 'methods' => [], ];
+$classes['GtkTextAttributes'] = ['c-name' => "GtkTextAttributes", 'methods' => [], ];
+$classes['GtkTextIter'] = ['c-name' => "GtkTextIter", 'methods' => [], ];
+$classes['GtkSymbolicColor'] = ['c-name' => "GtkSymbolicColor", 'methods' => [], ];
+$classes['GtkStock'] = ['c-name' => "GtkStock", 'methods' => [], ];
+// $classes['GtkRcParse'] = ['c-name' => "GtkRcParse", 'methods' => [], ];
+$classes['GtkRc'] = ['c-name' => "GtkRc", 'methods' => [], ];
+$classes['GtkIconSource'] = ['c-name' => "GtkIconSource", 'methods' => [], ];
+$classes['GtkIconSet'] = ['c-name' => "GtkIconSet", 'methods' => [], ];
+$classes['GtkIconSize'] = ['c-name' => "GtkIconSize", 'methods' => [], ];
+$classes['GtkTooltip'] = ['c-name' => "GtkTooltip", 'methods' => [], ];
+$classes['GtkTreePath'] = ['c-name' => "GtkTreePath", 'methods' => [], ];
+$classes['GtkTreeRowReference'] = ['c-name' => "GtkTreeRowReference", 'methods' => [], ];
+$classes['GtkTreeIter'] = ['c-name' => "GtkTreeIter", 'methods' => [], ];
+// $classes['GtkTree'] = ['c-name' => "GtkTree", 'methods' => [], ];
+$classes['GtkUIManager'] = ['c-name' => "GtkUIManager", 'methods' => [], ];
+$classes['GtkRequisition'] = ['c-name' => "GtkRequisition", 'methods' => [], ];
+$classes['GtkGradient'] = ['c-name' => "GtkGradient", 'methods' => [], ];
+$classes['GtkPaint'] = ['c-name' => "GtkPaint", 'methods' => [], ];
+$classes['GtkPrint'] = ['c-name' => "GtkPrint", 'methods' => [], ];
+$classes['Gtk'] = ['c-name' => "Gtk", 'methods' => [], ];
 
 
 /**
@@ -157,6 +254,36 @@ foreach($parsed['method'] as $method_name => $method) {
 	// add the method to the object
 	$classes[$class_name]['methods'][$method_name] = $method;
 }
+
+
+
+/**
+ * Loop between functions, looking for function that can be added as method of a class
+ */
+foreach($parsed['function'] as $function_name => $function) {
+	$camel_function = toCamelCase($function_name);
+
+	// if($function_name == "gtk_vbox_new") {
+	// 	die($camel_function);
+	// }
+	foreach($classes as $class_name => $class) {
+		
+		if(($pos = strpos($camel_function, $class_name)) !== FALSE) {
+
+			$method_name = substr($camel_function, strlen($class_name));
+			$method_name = explodeCamelCase($method_name);
+			$method_name = implode("_", array_map("strtolower", $method_name));
+
+			$classes[$class_name]['methods'][$method_name] = $function;
+
+			unset($parsed['function'][$function_name]);
+			break;
+		}
+
+	}
+}
+
+var_dump($parsed['function']);
 
 /**
  * Add classes to indexes
@@ -204,8 +331,6 @@ while($pos < count($classes)-1) {
 
 	$pos++;
 }
-
-
 
 
 
